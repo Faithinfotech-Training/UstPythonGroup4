1f718e3bddbc0653c03c9f0035ca8e75985e3dc0
manumohan95 <manumohan865@gmail.com>
2020-03-06 20:55:39 +0530

    commited

---
+++ app_package/course_forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm 
+from wtforms import StringField, SubmitField, IntegerField, RadioField, TextField
+from wtforms.validators import DataRequired, EqualTo, ValidationError , NumberRange
+
+
+class AddCourseForm(FlaskForm):
+    coursename=StringField("Course Name : ",validators=[DataRequired()])
+    courseduration=IntegerField("Course Duration:",validators=[DataRequired(),NumberRange(min=1)])
+    coursefee=IntegerField("Course Fee:",validators=[DataRequired(),NumberRange(min=1000)])
+    coursestatus=RadioField("Course Status:",choices = [('Active','Active'),('Inactive','Inactive')])
+    coursediscription=TextField("Course Discription:",validators=[DataRequired()])
+    submit=SubmitField("Add Course")
+
+class ModifyCourseForm(FlaskForm):
+    coursename=StringField("Course Name : ",validators=[DataRequired()])
+    courseduration=IntegerField("Course Duration:",validators=[DataRequired(),NumberRange(min=1)])
+    coursefee=IntegerField("Course Fee:",validators=[DataRequired(),NumberRange(min=1000)])
+    coursestatus=RadioField("Course Status:",choices = [('Active','Active'),('Inactive','Inactive')])
+    coursediscription=TextField("Course Discription:",validators=[DataRequired()])
+    submit=SubmitField("Modify Course")
