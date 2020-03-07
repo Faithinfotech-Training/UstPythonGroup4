a36dcc944e9eacd74c83347eb88d39d16b4ebf72
JeenuJayakumar <jeenujayakumar@gmail.com>
2020-03-07 17:29:20 +0530

    batch edited

--- app_package/batch_forms.py
+++ app_package/batch_forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateTimeField, RadioField, SubmitField
+from wtforms import StringField, IntegerField, DateTimeField, RadioField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class AddBatchForm(FlaskForm):  		
 	batch_name=StringField("Batch Name: ",validators=[DataRequired()])
 	start_date=DateTimeField("Start date: ", validators=[DataRequired()])	
-	end_date=DateTimeField("End date: ", validators=[DataRequired()])
-	course_id=IntegerField("Course ID: ", validators=[DataRequired()])   
+	end_date=DateTimeField("End date: ", validators=[DataRequired()])   
 	b_status=RadioField('Status: ', choices = [('Active','Active'),('Inactive','Inactive')])
+	coursename=SelectField("Course Name: ", coerce=str)
+	res_name=SelectField("Resource Name:" ,coerce=str)
 	submit=SubmitField("Submit")
 
 class ModifyBatchForm(FlaskForm):
